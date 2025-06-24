80fa3c9718f791a5386bd6ada3add8aaacaf6c6e
Kirill Bushmanov <53367565+dissin101@users.noreply.github.com>
2025-06-25 01:15:27 +0500

    Update base path

--- vite.config.cjs
+++ vite.config.cjs
@@ -5,7 +5,7 @@ const fs = require('fs')
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: '/experts',
+    base: '/',
     server: {
         host: '0.0.0.0',
         port: 3000,
